f9284a0ac3aa987b0694d7da5f26c3c9c17ae55b
Arseni Borisovs <arseni.borisovs@gmail.com>
2025-01-26 19:09:05 -0800

    Port to ff and remove redirect to gh-pages

--- content-script.js
+++ content-script.js
@@ -1,25 +1,36 @@
+console.log("Content script running");
 function deleteCommentAndLogo() {
-    try{
-        document.getElementsByClassName('_30BbATRhFv3V83DHNDjJAO')[0].innerHTML = ""
-        
+    try {
+        document.getElementsByClassName('_30BbATRhFv3V83DHNDjJAO')[0].innerHTML = "";
+
     }
-    catch(err) {
-        
+    catch (err) {
+
     }
 
-    try{
-        document.getElementsByClassName('_2l7c_Oz0UVsamALvPrlznq')[0].innerHTML = ""
+    try {
+        document.getElementsByClassName('_2l7c_Oz0UVsamALvPrlznq')[0].innerHTML = "";
     }
-    catch(err) {
+    catch (err) {
 
     }
 }
 
-chrome.storage.sync.get(['userData'], function(result) {
+function blockReddit() {
+    // 1) Build the local URL for blocked.html
+    //    chrome.runtime.getURL("blocked.html") returns something like:
+    //    moz-extension://<extension-id>/blocked.html in Firefox
+    const blockedUrl = chrome.runtime.getURL("blocked.html");
+
+    // 2) Redirect the current tab to that page
+    window.location.href = blockedUrl;
+}
+
+chrome.storage.local.get(['userData'], function (result) {
+    console.log("Result from local storage:", result);
     whiteList = [];
     blackList = [];
-    if (result.userData != undefined)
-    {
+    if (result.userData != undefined) {
         whiteList = result.userData.whiteList;
         blackList = result.userData.blackList;
     }
@@ -28,44 +39,37 @@ chrome.storage.sync.get(['userData'], function(result) {
     pathName = window.location.pathname;
 
     // check if not a comment section, and not a whitelisted subreddit
-    if (pathName != null )
-    {
-        if (pathName.includes("comments"))
-        {
+    if (pathName != null) {
+        if (pathName.includes("comments")) {
             // check the blacklist to see
             blackList.forEach(element => {
-                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase()))
-                {
+                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase())) {
                     //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-                    window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+                    blockReddit();
                 }
-            })
+            });
         }
-        else
-        {
+        else {
             console.log("2");
             // check each member of the whitelist, if the website isn't there, block it
             willBlock = true;
             whiteList.forEach(element => {
-                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase()))
-                {
+                if (pathName.toLowerCase().includes("/r/" + element.toLowerCase())) {
                     willBlock = false;
                 }
-            })
-            
+            });
+
             // I need to do testing to see if this works, because it could very well not work
-            if (willBlock)
-            {
+            if (willBlock) {
                 //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-                window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+                blockReddit();
             }
         }
     }
-    else
-    {
+    else {
         //document.getElementsByTagName('body')[0].innerHTML = "<k>STOP PROCRASTINATING ON REDDIT</k> <p>You may only go to reddit comment pages</p> <p>Extension built by <a style=\"color:blue\" href=\"https://henryz.dev/\">Henry Zhang</a></p>";
-        window.location.href = "https://hz757.github.io/PortfolioWebsite/RedditLiberationRedirect.html";
+        blockReddit();
     }
 
-    setInterval(deleteCommentAndLogo, 500)
+    setInterval(deleteCommentAndLogo, 500);
 });
